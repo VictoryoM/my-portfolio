05605ba14cbcab704aa2b6248ed6f34b9b466044
Victoryo <89630293+VictoryoM@users.noreply.github.com>
2024-02-21 06:59:42 +1300

    doneeee

--- src/app/components/ProjectTag.tsx
+++ src/app/components/ProjectTag.tsx
@@ -11,10 +11,10 @@ const ProjectTag = ({
 }) => {
   const buttonStyles = isSelected
     ? "text-white border-violet-700"
-    : "text-[#ADB7BE] border-slate-600 hover:border-white";
+    : "text-[#ADB7BE] border-slate-600 hover:border-white hover:shadow-white";
   return (
     <button
-      className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xs sm:text-sm md:text-lg lg:text-xl cursor-pointer shadow-violet-600 shadow-md transition-all duration-300 ease-in-out hover:border-violet-500 hover:shadow-violet-700 hover:scale-110 hover:rotate-12`}
+      className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xs sm:text-sm md:text-lg lg:text-xl cursor-pointer shadow-violet-600 shadow-md transition-all duration-300 ease-in-out hover:border-violet-500 hover:shadow-violet-700 hover:scale-110`}
       onClick={() => onClick(name)}
     >
       {name}
